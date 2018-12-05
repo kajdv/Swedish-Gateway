9a37810f7f7b5260aa84d74b99041db99e3ffa89
Kajsa Djarv <38329242+kajdv@users.noreply.github.com>
2018-12-05 15:31:40 -0500

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,21 +1,24 @@
 var shuffleSequence = seq("consent","setcounter","intro");
 //var practiceItemTypes = ["practice"];
 
+var showProgressBar = false;
+PennController.ResetPrefix(null);
 
 var items = [
 
     ["setcounter", "__SetCounter__", { } ] 
     ,    
-    ["consent", "PennController", PennController(
-        newHtml("consent", "ProlificConsent.html")
-            .settings.log()
-            .print()
-        ,
-        newButton("consent btn", "Jag g&aring;r med p&aring; att delta i denna studie.")
-            .print()
-            .wait( getHtml("consent").test.complete().failure( getHtml("consent").warn() ) )
-    )]
-    ,
+ //   ["consent", "PennController", PennController(
+ //       newHtml("consent", "ProlificConsent.html")
+ //           .settings.log()
+ //           .settings.css("max-width","40em")
+ //           .print()
+ //       ,
+ //       newButton("consent btn", "Jag g&aring;r med p&aring; att delta i denna studie.")
+ //           .print()
+ //           .wait( getHtml("consent").test.complete().failure( getHtml("consent").warn() ) )
+ //   )]
+ //   ,
 //    template
 //    [["intro", 1], "Form", {
 //        continueMessage: null,
@@ -25,50 +28,54 @@ var items = [
 //      manualSendResults: true,      
 //    }]
 //    ,
+
+
+// IS THIS WHAT I DO? And then just log PROLIFIC_PID also in the example_data of the individul files? And if it's empty it's ok?
+
     [["intro", 1], "Form", {
         continueMessage: null,
-        html:  "<a id='link'> Click here to begin the experiment!</a>"+
-               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_MCP_V2/experiment.html'+PennController.GetURLParameter("source"));</script>"} ,
+        html:  "<a id='link'> Klicka h&auml;r f&ouml;r att g&aring; vidare!</a>"+
+               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_MCP_V2/experiment.html?source='+PennController.GetURLParameter('source')+PennController.GetURLParameter('PROLIFIC_PID'));</script>"} ,
               "__SendResults__", {
       manualSendResults: true,  
     }]
     ,    
     [["intro", 1], "Form", {
         continueMessage: null,
-        html:  "<a id='link'> Click here to begin the experiment!</a>"+
-               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_MCP_SpAct/experiment.html'+PennController.GetURLParameter("source"));</script>"} ,
+        html:  "<a id='link'> Klicka h&auml;r f&ouml;r att g&aring; vidare!</a>"+
+               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_MCP_SpAct/experiment.html?source='+PennController.GetURLParameter('source')+PennController.GetURLParameter('PROLIFIC_PID'));</script>"} ,
               "__SendResults__", {
       manualSendResults: true,  
     }]
     ,     
     [["intro", 1], "Form", {
         continueMessage: null,
-        html:  "<a id='link'> Click here to begin the experiment!</a>"+
-               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_MCP_unm/experiment.html'+PennController.GetURLParameter("source"));</script>"} ,
+        html:  "<a id='link'> Klicka h&auml;r f&ouml;r att g&aring; vidare!</a>"+
+               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_MCP_unm/experiment.html?source='+PennController.GetURLParameter('source')+PennController.GetURLParameter('PROLIFIC_PID'));</script>"} ,
               "__SendResults__", {
       manualSendResults: true,  
     }]
     ,     
     [["intro", 1], "Form", {
         continueMessage: null,
-        html:  "<a id='link'> Click here to begin the experiment!</a>"+
-               "<script>document.getElementById('link').setAttribute('href', 'spellout.net/ibexexps/KD/Sw_DF_AHbel/experiment.html'+PennController.GetURLParameter("source"));</script>"} ,
+        html:  "<a id='link'> Klicka h&auml;r f&ouml;r att g&aring; vidare!</a>"+
+               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_DF_AHbel/experiment.html?source='+PennController.GetURLParameter('source')+PennController.GetURLParameter('PROLIFIC_PID'));</script>"} ,
               "__SendResults__", {
       manualSendResults: true,  
     }]
     , 
     [["intro", 1], "Form", {
         continueMessage: null,
-        html:  "<a id='link'> Click here to begin the experiment!</a>"+
-               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_DF_SpBel/experiment.html'+PennController.GetURLParameter("source"));</script>"} ,
+        html:  "<a id='link'> Klicka h&auml;r f&ouml;r att g&aring; vidare!</a>"+
+               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_DF_SpBel/experiment.html?source='+PennController.GetURLParameter('source')+PennController.GetURLParameter('PROLIFIC_PID'));</script>"} ,
               "__SendResults__", {
       manualSendResults: true,  
     }]
     ,      
     [["intro", 1], "Form", {
         continueMessage: null,
-        html:  "<a id='link'> Click here to begin the experiment!</a>"+
-               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_DF_DiscFam/experiment.html'+PennController.GetURLParameter("source"));</script>"} ,
+        html:  "<a id='link'> Klicka h&auml;r f&ouml;r att g&aring; vidare!</a>"+
+               "<script>document.getElementById('link').setAttribute('href', 'http://spellout.net/ibexexps/KD/Sw_DF_DiscFam/experiment.html?source='+PennController.GetURLParameter('source')+PennController.GetURLParameter('PROLIFIC_PID'));</script>"} ,
               "__SendResults__", {
       manualSendResults: true,  
     }]
